acf91b5851af29a71e7ee8ed76de6e2d4e6562b6
Elise-Beauvy <94043023+Elise-Beauvy@users.noreply.github.com>
2022-11-30 16:11:18 +0100

    change

--- Socket/client_socket.py
+++ Socket/client_socket.py
@@ -1,17 +1,33 @@
 import socket
+import time
+import multiprocessing
+import threading
 
 msgcl = ""
 msgsrv = ""
 
 host = socket.gethostname()
 
-client_socket = socket.socket()
-client_socket.connect((host, 10002))
+def client():
 
-while msgcl != "bye" and msgsrv != "bye" and msgcl != "arret" and msgsrv != "arret":
-    msgcl = input("Amon:")
-    client_socket.send(msgcl.encode())
-    msgsrv = client_socket.recv(1024).decode()
-    print(msgsrv)
-client_socket.close()
+    client_socket = socket.socket()
+    client_socket.connect((host, 10009))
 
+    msgsrv = ""
+    while msgsrv != "bye" and msgsrv != "arret":
+        msgsrv = client_socket.recv(1024).decode()
+        print(msgsrv)
+    client_socket.close()
+
+if __name__ == '__main__':
+    client_socket = socket.socket()
+    start = time.perf_counter()
+    p1 = threading.Thread(target=client(), args=[client_socket])
+    p1.start()
+
+    while msgcl != "bye" and msgcl != "arret":
+        msgcl = input("Amon:")
+        client_socket.send(msgcl.encode())
+
+    p1.join()
+    client_socket.close()
--- Socket/server_socket.py
+++ Socket/server_socket.py
@@ -1,20 +1,31 @@
 import socket
+import time
+import multiprocessing
+import threading
 
 msgsrv = ""
 msgcl = ""
 
 host = socket.gethostname()
+def serveur():
 
-server_socket = socket.socket()
-server_socket.bind((host, 10002))
-server_socket.listen(1)
-while msgcl != "arret" and msgsrv != "arret":
+    while msgcl != "bye" and  msgcl != "arret":
+            msgcl = conn.recv(1024).decode()
+            print(msgcl)
+
+if __name__ == '__main__':
+    server_socket = socket.socket()
+    server_socket.bind((host, 10009))
+    server_socket.listen(1)
     conn, address = server_socket.accept()
-    while msgcl != "bye" and msgsrv != "bye" and msgcl != "arret" and msgsrv != "arret":
 
-        msgcl = conn.recv(1024).decode()
-        print(msgcl)
-        msgsrv = input("Isis:")
+    start = time.perf_counter()
+    p1 = threading.Thread(target=serveur, args=[conn])
+    p1.start()
+
+    while msgsrv != "bye" and msgsrv != "arret":
+        msgsrv = str(input("Isis:"))
         conn.send(msgsrv.encode())
-    conn.close()
-server_socket.close()
+
+    p1.join()
+    server_socket.close()
